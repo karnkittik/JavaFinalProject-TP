8c9d77d109bf536770a8f4af455ec7a4ae903560
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2018-11-15 21:04:31 +0700

    try filling backgrund

--- KingTower/src/Main.java
+++ KingTower/src/Main.java
@@ -2,26 +2,44 @@
 import com.sun.javafx.fxml.builder.JavaFXImageBuilder;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	private static final String bgURL = "file:res/User.jpg";
+	
 	@Override
 	public void start(Stage primaryStage) {
-		HBox root = new HBox(10);
-		Button b = new Button("bbb");
-		//Image image = new Image(ClassLoader.getSystemResource("res/User.png").toString());
-		root.getChildren().addAll(b);
-		Scene scene = new Scene(root, 432.0, 768.0);
+		StackPane root1 = new StackPane();
+		StackPane root2 = new StackPane();
+		Button b1 = new Button("bbb");
+		Button b2 = new Button("ccc");
+		Image bg = new Image(bgURL);
+		root2.setStyle("-fx-background-image: url(file:res/User2.jpg);" );
+		root1.setStyle("-fx-background-image: url(file:res/User.jpg);" );
+		//Rectangle s = new Rectangle(60, 768, new ImagePattern(bg, 0, 0, 432, 768, false)); 
+		root1.getChildren().addAll(b1);
+		Scene scene1 = new Scene(root1, 600, 800);
+		Scene scene2 = new Scene(root2, 600, 800);
+		b1.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				primaryStage.setScene(scene2);
+			}
+		});
 		
-		primaryStage.setScene(scene);
+		primaryStage.setScene(scene1);
 		primaryStage.setTitle("KingTower");
-		primaryStage.setResizable(false);
+		//primaryStage.setResizable(false);
 		primaryStage.show();
 		
 		
